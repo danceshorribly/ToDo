cf69588da6fba853e8266cec41033f113196d343
Oscar Byhlinder <oscar@byhlinder.com>
2023-11-17 09:57:34 +0100

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Oscar Byhlinder on 2023-11-17.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
